8b877e49b59f35a22e0ba18f657fda0b752350df
ariankh <arian_l_100@hotmail.com>
2015-11-09 19:49:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 ### Launch server using gunicorn gunicorn, bind to all addresses
 ### gunicorn -k flask_sockets.worker --bind 0.0.0.0:8000 app:app
-$ heroku buildpacks:set https://github.com/heroku/heroku-buildpack-python
 from flask import Flask, render_template, redirect, request, g, session, url_for, flash, jsonify
 # import config
 import json
